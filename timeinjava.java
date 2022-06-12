eb02a9f5052c0ea31d40f1d5fff5063040a1e77b
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-12 10:03:52 -0300

    Update timeinjava.java

--- timeinjava.java
+++ timeinjava.java
@@ -8,24 +8,30 @@ public class timeinjava {
 
         int seconds = leitor.nextInt();
         minutes = seconds / 60;
-        int minutesprint = minutes % 60;
         int secondsprint = (int) (seconds % 60);
-        hours = minutes / 60;
+        if (minutes > 60) {
+            hours = minutes / 60;
+            minutes = minutes % 60;
 
-        if (hours > 24) {
+            if (hours > 24) {
 
-            int days = hours / 24;
-            hours = hours % 24;
-            System.out.printf(
-                    seconds + " seconds is equal to " + days + " days and " + hours + " hours and " + minutesprint
-                            + " minutes and " + secondsprint);
+                int days = hours / 24;
+                hours = hours % 24;
+                System.out.printf(
+                        seconds + " seconds is equal to " + days + " days and " + hours + " hours and " + minutes
+                                + " minutes and " + secondsprint);
+            } else {
+
+                System.out.println(
+                        seconds + " seconds is equal to " + hours + " hours and " + minutes + " minutes and "
+                                + secondsprint + "seconds");
+            }
+
+            leitor.close();
         } else {
-
             System.out.println(
-                    seconds + " seconds is equal to " + hours + " hours and " + minutesprint + " minutes and "
-                            + secondsprint + "seconds");
+                    seconds + " seconds is equal to " + minutes + " minutes and "
+                            + secondsprint + " seconds");
         }
-
-        leitor.close();
     }
 }
